78982e8f4848c3ccf37e1f9e3c6b1e5cc484c6aa
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-06-02 12:22:18 +0530

    Update notify_admins.py

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -3,12 +3,15 @@ import logging
 from aiogram import Dispatcher
 
 from data.config import ADMINS
-
+import datetime
 
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Bot ishga tushdi")
+            current_datetime = datetime.datetime.now()
+            date_time_str = current_datetime.strftime("%d/%m/%Y %H:%M:%S")
+
+            await dp.bot.send_message(admin, f"Bot Started at {date_time_str}")
 
         except Exception as err:
             pass
