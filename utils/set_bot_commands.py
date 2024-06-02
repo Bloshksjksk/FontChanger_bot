8a1250a1fb0c748846ae70682af850b94bf2d9bd
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-06-02 12:19:46 +0530

    Update set_bot_commands.py

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -21,25 +21,3 @@ async def set_default_commands(dp):
             """
         ]
     )
-
-from aiogram import types
-
-async def start_command(message: types.Message):
-    # Create an inline keyboard markup with a button
-    keyboard = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(text="ᴛʀᴜᴍʙᴏᴛꜱ", url="https://t.me/movie_time_botonly")
-    keyboard.add(button)
-
-    # Send a message with the text, photo, and inline keyboard
-    await message.answer_photo(
-        photo="",
-        caption="""ʜɪ ɪ ᴀᴍ ꜰᴏɴᴛ ᴄʜᴀɴɢᴇʀ ʙᴏᴛ
-
-        ᴍʏ ᴍᴇᴛʜᴏᴅꜱ :
-
-                1. ɪɴʟɪɴᴇ 
-
-                2.ᴅɪʀᴇᴄᴛ ᴍᴇꜱꜱᴀɢᴇ""",
-        reply_markup=keyboard
-    )
-
